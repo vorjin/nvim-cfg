b58fe6db67346dbcc7836a06fecc2b10b8b6ea1e
Dmytro Varhan <vorjin6272@gmail.com>
2026-08-18 14:20:10 +0300

    add grep cmds

--- lua/pack.lua
+++ lua/pack.lua
@@ -78,6 +78,8 @@ MiniExtra.setup()
 vim.keymap.set("n", "<leader>pf", function() MiniPick.builtin.files() end, { desc = "Mini File Picker" })
 vim.keymap.set("n", "<leader>ps", function() MiniPick.builtin.grep({ pattern = vim.fn.expand("<cword>") }) end,
     { desc = "Grep word/Search word" })
+vim.keymap.set("n", "<leader>pg", function() MiniPick.builtin.grep_live() end,
+    { desc = "Grep live (project)" })
 vim.keymap.set("n", "<leader>vh", function() MiniPick.builtin.help() end, { desc = "Mini Help" })
 
 vim.keymap.set("n", "<leader>xx", function() MiniExtra.pickers.diagnostic() end, { desc = "Mini Picker Diagnostics" })
